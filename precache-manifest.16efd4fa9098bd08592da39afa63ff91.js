e02108d1467e02b0774ea221262893e3b5056722
Leandro Bonfim <leandrobonfim15@homtial.com>
2022-04-02 19:17:17 -0300

    Updates

---
+++ precache-manifest.16efd4fa9098bd08592da39afa63ff91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd257861ea25a5e54511dcf7ef47b1e1",
+    "url": "/trybe-wallet/index.html"
+  },
+  {
+    "revision": "6e93e9123f579fd356fb",
+    "url": "/trybe-wallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1be4a9772a8b0277ae54",
+    "url": "/trybe-wallet/static/js/2.2baff6bb.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/trybe-wallet/static/js/2.2baff6bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e93e9123f579fd356fb",
+    "url": "/trybe-wallet/static/js/main.d5598de5.chunk.js"
+  },
+  {
+    "revision": "d390f7f33f2ac1ae0bcc",
+    "url": "/trybe-wallet/static/js/runtime-main.f94807e2.js"
+  }
+]);
