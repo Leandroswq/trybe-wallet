d7e3d89f0916cf2d4a75ca4ce66770e329e5a5f9
Leandro Bonfim <leandrobonfim15@homtial.com>
2022-04-03 12:36:46 -0300

    feat: created notlogged css

--- src/pages/NotLogged.js
+++ src/pages/NotLogged.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './css/NotLogged.css';
 
 export default function NotLogged() {
   return (
-    <div>
-      you are not logged in, to log in
-      {' '}
-      <Link to="/">click here.</Link>
+    <div className="container-centralized background-color-prymary ">
+      <div className="Not-logged ">
+        <p className="font-size-xx">
+          You are not logged
+        </p>
+        <Link className="font-size-xx Not-logged__link" to="/">Click here</Link>
+        <span className="font-size-xx">
+          {' '}
+          to login
+        </span>
+      </div>
     </div>);
 }
